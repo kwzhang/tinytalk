82eaa244edf909d84f73f73c9151c832d96e6bd7
bojun.shim <bojun.shim@lge.com>
2018-05-17 01:02:48 +0900

    [server] initial commit

---
+++ server/src/main/java/com/ssfp/user/UserDao.java
@@ -0,0 +1,13 @@
+package com.ssfp.user;
+
+import io.swagger.model.User;
+
+public class UserDao {
+	public static void register(User user) {
+		System.out.println("register");
+		System.out.println("email: " + user.getEmail());
+		System.out.println("number: "  + user.getPhone());
+		System.out.println("firstName: " + user.getFirstName());
+		System.out.println("lastName: " + user.getLastName());
+	}
+}
---
+++ server/src/main/java/com/ssfp/user/UserDaoTest.java
@@ -0,0 +1,14 @@
+package com.ssfp.user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoTest {
+
+	@Test
+	public void testRegister() {
+		fail("User class register test");
+	}
+
+}
