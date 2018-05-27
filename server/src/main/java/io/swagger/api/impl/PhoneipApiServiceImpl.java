540d94cbf116054cd9b1e809c1b36a0b24d8717f
bojun.shim <bojun.shim@lge.com>
2018-05-28 01:32:29 +0900

    [server]Add phoneip implementation example

--- server/src/main/java/io/swagger/api/impl/PhoneipApiServiceImpl.java
+++ server/src/main/java/io/swagger/api/impl/PhoneipApiServiceImpl.java
@@ -1,26 +1,20 @@
 package io.swagger.api.impl;
 
-import io.swagger.api.*;
-import io.swagger.model.*;
-
-import io.swagger.model.PhoneIp;
-
-import java.util.List;
-import io.swagger.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-import javax.validation.constraints.*;
+
+import io.swagger.api.NotFoundException;
+import io.swagger.api.PhoneipApiService;
+import io.swagger.model.PhoneIp;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-05-27T15:54:36.606Z")
 public class PhoneipApiServiceImpl extends PhoneipApiService {
     @Override
     public Response phoneip(String phonenumber, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-    	System.out.println(phonenumber.toString());
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	System.out.println("phonenumber:" + phonenumber.toString());
+//        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	PhoneIp ip = new PhoneIp();
+    	ip.setIp("192.168.0.100");
+        return Response.ok().entity(ip).build();
     }
 }
