7bc41cc62701aa76cfdead4d3e28e5bbec877e19
kwzhang <infestedzhang@gmail.com>
2018-06-26 21:49:52 -0400

    Reschedule for conference call

--- client/app/src/main/java/com/lge/architect/tinytalk/command/model/ConferenceCallSchedule.java
+++ client/app/src/main/java/com/lge/architect/tinytalk/command/model/ConferenceCallSchedule.java
@@ -1,20 +1,24 @@
 package com.lge.architect.tinytalk.command.model;
 
 import org.joda.time.DateTime;
-import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.util.List;
 
 public class ConferenceCallSchedule {
   public static final String URI = "ccrequest";
 
+  private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+  private static DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(DATETIME_FORMAT);
+
   private List<String> members;
-  private DateTime startDatetime;
-  private DateTime endDatetime;
+  private String startDatetime;
+  private String endDatetime;
 
   public ConferenceCallSchedule(List<String> members, DateTime startDatetime, DateTime endDatetime) {
     this.members = members;
-    this.startDatetime = startDatetime;
-    this.endDatetime = endDatetime;
+    this.startDatetime = dateTimeFormatter.print(startDatetime);
+    this.endDatetime = dateTimeFormatter.print(endDatetime);
   }
 }
